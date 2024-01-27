1870716709cddb64b99820a33bc98ed62fcde54e
nebrosarth <etozhemadd@bk.ru>
2024-01-27 14:28:46 +0500

    Improve UI, add splitters synchronization

--- TestApp/TestApp.cpp
+++ TestApp/TestApp.cpp
@@ -5,6 +5,16 @@ TestApp::TestApp(QWidget *parent)
     , ui(new Ui::TestAppClass())
 {
     ui->setupUi(this);
+
+    ui->splitter->setSizes(QList<int>({ INT_MAX, INT_MAX }));
+    ui->splitter_2->setSizes(QList<int>({ INT_MAX, INT_MAX }));
+
+    connect(ui->splitter, &QSplitter::splitterMoved, ui->splitter_2, [&] {
+        ui->splitter_2->setSizes(ui->splitter->sizes());
+        });
+    connect(ui->splitter_2, &QSplitter::splitterMoved, ui->splitter, [&] {
+        ui->splitter->setSizes(ui->splitter_2->sizes());
+        });
 }
 
 TestApp::~TestApp()
